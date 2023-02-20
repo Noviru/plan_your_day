c29a19259da710aec58fbd7984ca3916a98010db
Gustav Fridén <gusfride@gmail.com>
2023-02-20 12:43:43 +0100

    Made functions for checking if date is correct

--- keeptrack/lib/DateFunctions.ts
+++ keeptrack/lib/DateFunctions.ts
@@ -9,17 +9,6 @@ export function is_number(value: string): boolean {
 }
 
 /**
- * Checks if the year provided is in the correct range and if the year is a number
- * 
- * @param year - The year you want to check 
- * @returns - True or false whether the year is in range or not and is a number or not 
- */
-export function check_year(year: string): boolean {
-    return is_number(year) && Number(year) < 2100 && Number(year) > get_todays_year();
-}
-
-
-/**
  * Returns the date for today
  * 
  * @returns Todays date
@@ -33,8 +22,108 @@ export function get_todays_date(): Date {
  * 
  * @returns - current year
  */
-export function get_todays_year(): Number {
+export function current_year(): number {
     return get_todays_date().getFullYear();
+}
+/**
+ * Returns the current month
+ * 
+ * @returns - current month
+ */
+export function current_month(): number {
+    return get_todays_date().getMonth();
+} 
+
+/**
+ * Returns the current day
+ * 
+ * @returns - current day
+ */
+export function current_day(): number {
+    return get_todays_date().getDay();
+}
+
+
+export function binary_search<T>(value: T, arr: Array<T>): boolean {
+    let index = 0;
+    let limit = arr.length - 1;
+    while (index <= limit) {
+        const pointer = Math.ceil((index + limit) / 2);
+        const entry = arr[pointer];
+        if (value > entry) {
+            index = pointer + 1;
+            continue;
+        } else if (value < entry) {
+            limit = pointer - 1;
+            continue;
+        }
+        return true; 
+    }
+  return false;
+};
+
+/**
+ * Checks if the year provided is in the correct range and if the year is a number
+ * 
+ * @param year - The year you want to check 
+ * @returns - True or false whether the year is in range or not and is a number or not 
+ */
+export function check_year(year: string): boolean {
+    return is_number(year) && Number(year) <= 2100 && Number(year) >= current_year();
+}
+
+
+
+export function day_in_month(month: string, day: string): boolean {
+    const months_with_30_days: Array<number> = [1, 3, 5, 7, 8, 10, 12];
+    const months_with_31_days: Array<number> = [4, 6, 9, 11];
+    if (is_number(month) && is_number(day) && Number(day) >= 1) {
+        if (binary_search(Number(month), months_with_30_days) && Number(day) <= 30) {
+            return true;
+        } else if (binary_search(Number(month), months_with_31_days) && Number(day) <= 31) {
+            return true;
+        } else if (Number(month) === 2 && Number(day) <= 28) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+export function leap_year_check(year: string, month:string, day: string): boolean {
+    return is_number(year) && is_number(month) && is_number(day) && 
+           Number(year) % 4 === 0 && Number(month) === 2 && Number(day) === 29;
+}
+
+export function date_has_passed(year: string, month: string, day: string): boolean {
+    const curr_year = current_year();
+    const curr_month = current_month();
+    const curr_day = current_day();
+    if (Number(year) < curr_year) {
+        return true;        
+    } else if (Number(year) === curr_year && Number(month) < curr_month) {
+        return true;
+    } else if (Number(year) === curr_year && Number(month) === curr_month && Number(day) < curr_day) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+export function check_date(date: string): boolean {
+    const date_arr: Array<string> = date.split("-", 3);
+    const year: string = date_arr[0];
+    const month: string = date_arr[1];
+    const day: string = date_arr[2];
+    if (check_year(year) && (day_in_month(month, day) || leap_year_check(year, month, day)) 
+        && date.length === 10 && !date_has_passed(year, month, day)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
