1ab90f501d0b3700c5a04ffc598342cca10be1d4
Gustav Fridén <gusfride@gmail.com>
2023-02-20 13:25:33 +0100

    Added function specifictions

--- keeptrack/lib/DateFunctions.ts
+++ keeptrack/lib/DateFunctions.ts
@@ -43,8 +43,14 @@ export function current_day(): number {
     return get_todays_date().getDay();
 }
 
-
-export function binary_search<T>(value: T, arr: Array<T>): boolean {
+/**
+ * a searching algorithm used in a sorted array by repeatedly dividing the search interval in half
+ * 
+ * @param value - number to search for
+ * @param arr - array with numbers
+ * @returns - true or false whether the value is in the array
+ */
+export function binary_search(value: number, arr: Array<number>): boolean {
     let index = 0;
     let limit = arr.length - 1;
     while (index <= limit) {
@@ -66,14 +72,20 @@ export function binary_search<T>(value: T, arr: Array<T>): boolean {
  * Checks if the year provided is in the correct range and if the year is a number
  * 
  * @param year - The year you want to check 
- * @returns - True or false whether the year is in range or not and is a number or not 
+ * @returns - True or false whether the year is in range or not
  */
 export function check_year(year: string): boolean {
     return is_number(year) && Number(year) <= 2100 && Number(year) >= current_year();
 }
 
 
-
+/**
+ * Checks if the input day is included in input month
+ * 
+ * @param month - the month we want to check for
+ * @param day - the day we want to check for
+ * @returns - true or false whether the day exists in the month or not
+ */
 export function day_in_month(month: string, day: string): boolean {
     const months_with_30_days: Array<number> = [1, 3, 5, 7, 8, 10, 12];
     const months_with_31_days: Array<number> = [4, 6, 9, 11];
@@ -92,11 +104,28 @@ export function day_in_month(month: string, day: string): boolean {
     }
 }
 
+/**
+ * Checks if input date is february 29th in a leap year
+ * 
+ * @param year - year to check if its a leap year
+ * @param month - month to check if its february
+ * @param day - day to check if it's the 29th
+ * @returns - true or false whether the input date is february 29th in a leap your or not
+ */
 export function leap_year_check(year: string, month:string, day: string): boolean {
     return is_number(year) && is_number(month) && is_number(day) && 
            Number(year) % 4 === 0 && Number(month) === 2 && Number(day) === 29;
 }
 
+
+/**
+ * Checks if input date already has passed
+ * 
+ * @param year - year of input date
+ * @param month - month of input date
+ * @param day - day of input date
+ * @returns - true or false whether input date has passed or not
+ */
 export function date_has_passed(year: string, month: string, day: string): boolean {
     const curr_year = current_year();
     const curr_month = current_month();
@@ -113,6 +142,13 @@ export function date_has_passed(year: string, month: string, day: string): boole
 
 }
 
+
+/**
+ * Checks if input date is correctly written or not
+ * 
+ * @param date - date to check
+ * @returns - boolean whether input date is correct or not
+ */
 export function check_date(date: string): boolean {
     const date_arr: Array<string> = date.split("-", 3);
     const year: string = date_arr[0];
